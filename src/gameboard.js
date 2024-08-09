ae4c914338c36eb5f0048edc73e7dd196b8d5027
tptam <99264397+tptam@users.noreply.github.com>
2024-08-09 22:41:12 +0900

    feat: add placeShip function

---
+++ src/gameboard.js
@@ -0,0 +1,24 @@
+import { Ship } from "./ship.js";
+
+export const Gameboard = () => {
+  const board = Array(10).fill(Array(10).fill(null));
+
+  const placeShip = (x, y, length, direction) => {
+    let cells;
+    if (direction === "vertical") {
+      cells = [...Array(length).keys()].map((val) => ({ x: x, y: y + val }));
+    } else {
+      cells = [...Array(length).keys()].map((val) => ({ x: x + val, y: y }));
+    }
+    if (cells.every((cell) => board[cell.x][cell.y] === null)) {
+      const ship = Ship(length);
+      cells.forEach((cell) => (board[cell.x][cell.y] = ship));
+    }
+  };
+
+  return {
+    missedAttacks: [],
+    areAllShipsSunk: () => false,
+    placeShip,
+  };
+};
